a4bbe3d6f7c6feb56f904d03497f44f11eefa97f
Curtis Jochen <curtis.jochen@gmail.com>
2020-04-29 11:24:08 -0400

    Learned about debugging tool in VS Code. Created if statement for user input on the command line

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,9 +4,16 @@ namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) /*Main is passing an array of strings as args*/
         {
-            Console.WriteLine("Hello Curtis!");
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
