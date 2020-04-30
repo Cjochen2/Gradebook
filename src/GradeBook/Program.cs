dfdb84a2d3fdedbb49d524345177cc9f787749a4
Curtis Jochen <curtis.jochen@gmail.com>
2020-04-30 09:57:54 -0400

    Created a calculation for averaging. Learned syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,21 @@ namespace GradeBook
     {
         static void Main(string[] args) /*Main is passing an array of strings as args*/
         {
+            /*var numbers = new[] { 88.7, 99.7, 69.5 }; This is initializing an array with these three numbers*/
+
+            var grades = new List<double>() { 88.7, 99.7, 69.5 }; /*This is initializing a List type from the System.Collections.Generic namspace with these three numbers*/
+
+            var results = 0.0;
+
+            foreach (var number in grades)
+            {
+                results += number;
+
+                results = results/grades.Count;
+            }
+
+            Console.WriteLine(results);
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
