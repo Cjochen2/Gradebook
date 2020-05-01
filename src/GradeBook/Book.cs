35a219e8bdb7e251dd4f537fa44f8468df203ce1
Curtis Jochen <curtis.jochen@gmail.com>
2020-04-30 20:21:51 -0400

    Finished module 4. Created a new class and methods to abstract and encapsulate the more complex code

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        /* This is an explicit constructor method. It needs to have the same name as the class and no return type */
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var results = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+
+                results += number;
+            }
+            results /= grades.Count;
+
+            Console.WriteLine($"The average grade is {results:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,31 +5,13 @@ namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args) /*Main is passing an array of strings as args*/
+        static void Main(string[] args)
         {
-            /*var numbers = new[] { 88.7, 99.7, 69.5 }; This is initializing an array with these three numbers*/
-
-            var grades = new List<double>() { 88.7, 99.7, 69.5 }; /*This is initializing a List type from the System.Collections.Generic namspace with these three numbers*/
-
-            var results = 0.0;
-
-            foreach (var number in grades)
-            {
-                results += number;
-
-                results /= grades.Count;
-            }
-
-            Console.WriteLine($"The average grade is {results:N1}");
-
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+            var book = new Book("Curtis' Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
         }
     }
 }
